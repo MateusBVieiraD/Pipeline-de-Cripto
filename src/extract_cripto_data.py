a68ea5b33778d40fa2d3722f1c0a982b9ce319b8
MateusBVieiraD <mateusvieira.duarte@outlook.com>
2026-05-12 17:51:36 -0300

    feat: adiciona a função de tranformações.

--- src/extract_cripto_data.py
+++ src/extract_cripto_data.py
@@ -6,6 +6,14 @@ def extract_bitcoin_data(url: str, cab:dict[str, str]):
     response = requests.get(url, headers=cab)
     data = response.json()
 
+    if response.status_code != 200:
+        print(f"Houve erro na requisição, status code: {response.status_code}.")
+        return []
+    
+    if not data:
+        print("A a lista de criptos retornou vazia.")
+        return []
+
     output_path = 'data/btc_info.json'
     output_dir = Path(output_path).parent
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -13,6 +21,5 @@ def extract_bitcoin_data(url: str, cab:dict[str, str]):
     with open(output_path, 'w') as f:
         json.dump(data, f, indent=2)
 
-    print(f'arquivo salvo em {output_path}')
+    print(f'Arquivo salvo em {output_path}.')
     return data
-    
